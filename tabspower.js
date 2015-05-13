81d1ca32ddd262fbeb339d908a482cc09ef47bfa
melnikov <melnikov@ttcsoft.ru>
2015-05-13 11:11:21 +0300

    Tabs jQuery plugin

---
+++ tabspower.js
@@ -0,0 +1,63 @@
+$.fn.tabsPower = function () {
+    
+    /*
+    Обязательные классы и атрибуты:
+      js-tab-menu - контейнер с табами
+      js-tab-menu-btn - таб-кнопка
+        data-tab-btn - атрибут для таба-кнопки
+      js-tab-content - контейнер с отображаемыми элементами табов
+      js-tab-item - элемент таба для скрытия/отображения
+        data-tab-item - атрибут для элемента таба
+
+    */
+    
+    // poweredGrp - объект, к которому применяем плагин
+    var poweredGrp = $(this),
+      allTabItems = poweredGrp.find('.js-tab-item').hide();
+
+    function tabs(allTabItems, clickedBtn) {
+
+      // для срабатывания клика только у неактивных табов
+      if ( !(clickedBtn.hasClass('active')) ) {
+
+        var curBtnData = clickedBtn.data('tab-btn'),
+            allCurItems = poweredGrp.find('.js-tab-content').find('.js-tab-item');
+
+        clickedBtn.addClass('active')
+          .siblings().removeClass('active');
+
+        // если нет дата = all, то показывает все элементы
+        if ( clickedBtn.data('tab-btn') === 'all' ) {
+          allCurItems.fadeIn();
+        } else {
+          allCurItems.hide();
+
+          // перебор атрибутов дат элементов меню с кликнутым табом и открытие кликнутого
+          allCurItems.each(function(){
+            if ( $(this).data('tab-item') === curBtnData ) {
+              $(this).fadeIn();
+            }
+          });
+
+        }
+
+      }
+    }
+
+    // событие на клик у кнопки в элементе js-tab-menu
+    poweredGrp.find('.js-tab-menu').on('click', '.js-tab-menu-btn', function (e) {
+      e.preventDefault();
+      var $this = $(this);
+      tabs(allTabItems, $this);
+    });
+
+    // эмуляция клика по первому табу при загрузке страницы
+    poweredGrp.find('.js-tab-menu').each(function(){
+      $(this).find('.js-tab-menu-btn').first().click();
+    });
+
+  };
+  //  $.fn.tabsPower 
+
+  // вызов функции
+  //$(*tab-container*).tabsPower();
